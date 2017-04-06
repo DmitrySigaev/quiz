f2d234ada88d62493bde552f8d602b2864c5be41
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2017-04-06 17:38:27 +0300

    I added to exam.sh items that check compilations with -0x flags. Outputs results are diffent. Please see out_gcc_ox.md files.

--- quiztest/exam.sh
+++ quiztest/exam.sh
@@ -3,3 +3,11 @@
 # My first bash
 g++ exam1.cpp -std=c++11 
 ./a.out >>out_gcc_default.md
+g++ -O0 exam1.cpp -std=c++11 
+./a.out >>out_gcc_o0.md
+g++ -O1 exam1.cpp -std=c++11 
+./a.out >>out_gcc_o1.md
+g++ -O2 exam1.cpp -std=c++11 
+./a.out >>out_gcc_o2.md
+g++ -O3 exam1.cpp -std=c++11 
+./a.out >>out_gcc_o3.md
